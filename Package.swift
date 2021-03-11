f8792c0a193c7accebbeaae93adfd492e84e73ff
DrJackyl <github@drjackyl.de>
2021-03-11 22:41:20 +0100

    Add DJEncoding as dependency to actual target
    * It probably worked, because DJEncoding was a dependency for the test-target.
    * Weird, that I missed it though.
    * Add .gitignore

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DJiRData",
-            dependencies: []),
+            dependencies: ["DJEncoding"]),
         .testTarget(
             name: "DJiRDataTests",
             dependencies: ["DJiRData", "DJEncoding"],
