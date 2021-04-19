bfb13cad044b1c90f041f1a13dda9b99326bcd34
DrJackyl <github@drjackyl.de>
2021-04-19 19:38:33 +0200

    Update README and improve style of unit-tests, which execute throwing functions

--- Tests/DJiRDataTests/DJiRData+CSVSeasonStandings.swift
+++ Tests/DJiRDataTests/DJiRData+CSVSeasonStandings.swift
@@ -5,36 +5,27 @@ import Foundation
 
 class DJiRDataCSVSeasonStandingsTests: XCTestCase {
     
-    func test_StandingsOfAWeek() {
+    func test_StandingsOfAWeek() throws {
         let data = DJiRDataTestResources
             .CSVSeasonStandings
             .SeasonStandings_Week
             .load()
         
-        do {
-            let csvSeasonStandings = try DJiRData().createCSVSeasonStandingsFromData(data)
-            XCTAssertEqual(csvSeasonStandings.count, 680)
-            XCTAssertEqual(csvSeasonStandings.filter { $0.clubname == "DE-AT-CH" }.count, 101)
-            XCTAssertTrue(csvSeasonStandings.contains { $0.name == "Felix Lieb" })
-        } catch let error {
-            XCTFail("An error was thrown: \(error)")
-        }
+        let csvSeasonStandings = try DJiRData().createCSVSeasonStandingsFromData(data)
+        XCTAssertEqual(csvSeasonStandings.count, 680)
+        XCTAssertEqual(csvSeasonStandings.filter { $0.clubname == "DE-AT-CH" }.count, 101)
+        XCTAssertTrue(csvSeasonStandings.contains { $0.name == "Felix Lieb" })
     }
     
-    func test_StandingsOfASeason() {
+    func test_StandingsOfASeason() throws {
         let data = DJiRDataTestResources
             .CSVSeasonStandings
             .SeasonStandings_Season
             .load()
-        
-        do {
-            let csvSeasonStandings = try DJiRData().createCSVSeasonStandingsFromData(data)
-            XCTAssertEqual(csvSeasonStandings.count, 2461)
-            XCTAssertEqual(csvSeasonStandings.filter { $0.clubname == "DE-AT-CH" }.count, 322)
-            XCTAssertTrue(csvSeasonStandings.contains { $0.name == "Felix Lieb" })
-        } catch let error {
-            XCTFail("An error was thrown: \(error)")
-        }
+        let csvSeasonStandings = try DJiRData().createCSVSeasonStandingsFromData(data)
+        XCTAssertEqual(csvSeasonStandings.count, 2461)
+        XCTAssertEqual(csvSeasonStandings.filter { $0.clubname == "DE-AT-CH" }.count, 322)
+        XCTAssertTrue(csvSeasonStandings.contains { $0.name == "Felix Lieb" })
     }
     
     func test_EmptyData() {
